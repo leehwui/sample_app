acd0ac4433bf8cd37ff74725af65789e52376ccc
leehwui <leehwui@163.com>
2015-08-12 21:50:49 +0800

    Finished chapter8

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'rails', '4.2.1'
 
 gem 'bootstrap-sass'
 gem 'bcrypt'
-
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'user/new'
@@ -13,7 +15,16 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup' => 'users#new'
+  
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+#  post 'logout' => 'sessions#destroy'
+  delete 'logout' => 'sessions#destroy'
+
+  
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
